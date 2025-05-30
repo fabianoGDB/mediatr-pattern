0a3e19fbef871516ba5c43a9cfade7b8da2711de
fabianoGDB <doctorfabi.f@gmail.com>
2025-05-30 04:39:01 -0300

    A006: creating the Meditor and Abstractions and a sample

---
+++ complete-version/Mediator.Samples/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mediator.Abstractions;
+using Mediator.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+
+var services = new ServiceCollection();
+services.AddMediator(typeof(Program).Assembly);
+services.AddTransient<AccountRepository>();
+
+var sevicesProvider = services.BuildServiceProvider();
+var mediator = sevicesProvider.GetService<IMediator>();
+
+var repository = new AccountRepository();
+var request = new CreateAccountCommand { Username = "admin", Password = "123123" };
+var result = await mediator.SendAsync(request);
+
+Console.WriteLine(result);
+public class AccountRepository
+{
+    public void Save()
+    {
+        Console.WriteLine("Saving...");
+    }
+}
+
+public class CreateAccountCommand : IRequest<string>
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class CreateAccountHandler(AccountRepository repository) : IHandler<CreateAccountCommand, string>
+{
+    public Task<string> HandleAsync(CreateAccountCommand request, CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine($"Creating {request.Username} account...");
+        repository.Save();
+        return Task.FromResult($"Account {request.Username} created");
+    }
+}
---
+++ complete-version/Mediator/Extensions/MediatorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Mediator.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Mediator.Extensions
+{
+    public static class MediatorExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddTransient<IMediator, Mediator>();
+
+            var handlerType = typeof(IHandler<,>);
+
+            foreach (var assembly in assemblies)
+            {
+                var handlers = assembly.GetTypes()
+                        .Where(type => !type.IsAbstract == false && type.IsInterface)
+                        .SelectMany(x => x.GetInterfaces(), (t, i) => new { Type = t, Interface = i })
+                        .Where(ti => ti.Interface.IsGenericType && ti.Interface.GetGenericTypeDefinition() == handlerType);
+
+                foreach(var handler in handlers)
+                {
+                    services.AddTransient(handler.Interface, handler.Type);
+                }
+            }
+
+            return services;
+        }
+    }
+}
